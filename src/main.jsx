6642a4163a0e909b91a6e31b68c599a798c29fb7
cohengmc <34006245+cohengmc@users.noreply.github.com>
2024-11-06 11:18:03 -0500

    map kinda working

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import App from "./App";
+import { scaleFactor } from "./constants";
+import { displayDialogue } from "./utils";
 import { k } from "./kaboomCtx";
+
 
 k.loadSprite("spritesheet", "./spritesheet.png", {
   sliceX: 39,
@@ -17,10 +21,96 @@ k.loadSprite("spritesheet", "./spritesheet.png", {
 
 k.loadSprite("map", "./map.png");
 
-k.setBackground(k.Color.fromHex("#311047"))
+k.setBackground(k.Color.fromHex("#87CEEB"));
 
-// createRoot(document.getElementById("root")).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>
-// );
+//Define Scene
+k.scene("main", async () => {
+  const mapData = await (await fetch("./map.json")).json();
+  const layers = mapData.layers; // Layers are the different map layers (boundaries)
+
+  // Game Object (objects: players, props, etc. object that contains components: positions, area, sprite, etc.)
+  // Make allows you to make a game object but not display it on the screen
+  // Add adds it to screen.
+  // So either Make -> Add or just Add
+  //.sprite -> // Sepcificy key you want sprite to display
+  //.pos -> pos the game object will have on screen if it is displayed
+  //.scalre -> scale up because pixel art is small and we want to make it bigger
+  const map = k.add([k.sprite("map"), k.pos(0), k.scale(scaleFactor)]);
+
+  const player = k.make([
+    // passing the spritesheet with a default animation (specified above in ./loadSprite)
+    k.sprite("spritesheet", { anim: "idle-down" }),
+
+    //.area -> creates hitbox for our game object (in this case player)
+    k.area({
+      //default is rough shape of sprite but we want more specificity
+      shape: new k.Rect(k.vec2(0, 3), 10, 10),
+    }),
+    //.body -> amkes it a tangible physics object
+    k.body(),
+    //.anchor-> have x,y correspond to center as opposed to top left
+    k.anchor("center"),
+    k.pos(),
+    k.scale(scaleFactor),
+
+    //hold properties within an game object by passing in an object with any properties you want. so player.speed = 250 but we can make anything like adding hand: "left" then player.hand would be left
+    {
+      speed: 250,
+      direction: "down",
+      isInDialogue: false,
+    },
+    // tag for this game object - way to identify the game object
+    "player",
+  ]);
+
+  for (const layer of layers) {
+    if (layer.name === "boundaries") {
+      for (const boundary of layer.objects) {
+        map.add([
+          k.area({
+            shape: new k.Rect(k.vec2(0), boundary.width, boundary.height),
+          }),
+          k.body({ isStatic: true }),
+          k.pos(boundary.x, boundary.y),
+          boundary.name,
+        ]);
+
+        if (boundary.name) {
+          player.onCollide(boundary.name, () => {
+            player.isInDialogue = true;
+
+            displayDialogue("TODO", () => {
+              player.isInDialogue = false;
+            });
+          });
+        }
+      }
+      //continue;
+    }
+
+    if (layer.name === "spawnpoint") {
+      for (const entity of layer.objects) {
+        if (entity.name === "player") {
+          player.pos = k.vec2(
+            (map.pos.x + entity.x) * scaleFactor,
+            (map.pos.y + entity.y) * scaleFactor
+          );
+          k.add(player);
+        }
+      }
+    }
+  }
+
+  k.onUpdate(() => {
+    k.camPos(player.pos.x, player.pos.y + 100);
+  });
+});
+
+// Default Scene - go to Scene - Scene "Main"
+k.go("main");
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
