b2cd33bba137398bd2cba2294d61d4f0ae1606c3
cohengmc <34006245+cohengmc@users.noreply.github.com>
2024-11-06 14:45:33 -0500

    adding favicon and castle for first portfolio branch

--- src/constants.jsx
+++ src/constants.jsx
@@ -4,4 +4,9 @@ export const dialogueData = {
   key: "You found a key!",
   coin: "Money!!",
   computer: `Wow you found <a href="https://www.youtube.com" target="_blank">YouTube</a>!`,
+  doorEngineering: `Want to see Geoff's Engineering Projects? 
+    <div class="btn-container">
+        <button id="close" class="ui-btn">Yeah!</button>
+        <button id="close" class="ui-btn">No thanks.</button>
+     </div>`,
 };
